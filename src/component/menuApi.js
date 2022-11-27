c6e04ffa24bae20ad387e7a5db387c967e5a1f32
sarita devkar <devkarsarita012@gmail.com>
2022-11-27 12:06:11 +0530

    first commit

---
+++ src/component/menuApi.js
@@ -0,0 +1,80 @@
+const Menu = [{
+    id:1,
+    image: "images/maggi.jpg",
+    name: "maggie",
+    category: "Breakfast",
+    price:"Rs.25",
+    description: "Enjoy Cheese MAGGI Noodles, Paneer MAGGI Noodles, Egg MAGGI Noodles, Veg MAGGI Noodles that add a twist of flavour and yumminess to your everyday MAGGI Noodles. Recipes 21 Veggie Delight MAGGI Noodles Recipe 95 of 100 35 Min Easy Paneer Taka Tak MAGGI Noodles Recipe 55 of 100 36 Min Easy Paneer ...",
+},
+
+
+{
+    id: 2,
+    image: "../images/samosa.jpg",
+    name: "samaso",
+    category: "evening",
+    price: "Rs 10",
+    description:
+      " Samosas are very delicious and a mouth watering snack. ",
+  },
+  {
+    id: 3,
+    image: "images/vadapaav.jpg",
+    name: "Vadapav",
+    category: "breakfast",
+    price: "Rs 10",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 4,
+    image: "../images/chole-bhature.jpg",
+    name: "chole Bhature",
+    category: "lunch",
+    price: "Rs 50",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 5,
+    image: "../images/pizza.jpg",
+    name: "pizza",
+    category: "evening",
+    price: "Rs 80",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 6,
+    image: "../images/non-veg_thali.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "Rs 180",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 7,
+    image: "../images/gulabjamun.jpg",
+    name: "Gulabjamun",
+    category: "dinner",
+    price: "Rs. 30",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 8,
+    image: "../images/rajma-chawal.jpg",
+    name: "Rajma chawal",
+    category: "lunch",
+    price: "60₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+
+];
+
+
+
+export default Menu;

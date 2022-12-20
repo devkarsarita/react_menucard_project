53b20e72f21dd4c3aecae62f713b7859c9aeec7f
sarita devkar <devkarsarita012@gmail.com>
2022-12-20 14:26:52 +0530

    add new items in menucard

--- src/component/MenuCard.js
+++ src/component/MenuCard.js
@@ -6,28 +6,30 @@ const MenuCard = ({ menuData }) => {
     <>
     <section className="main-card--container">
       {menuData.map((curElem) => {
+         const {id, name, category, image, description } = curElem;
+            //  Array destructuring
         return (
        <>
-      <div className='card-container' key={curElem.id}>
+      <div className='card-container' key={id}>
        <div className='card'>
           <div className='card-body'>
               <span className='card-number card-circle subtle'>
-              {curElem.id}
+              {id}
               </span>
-               <span className="card-author subtle">{curElem.category}</span>
-               <h2>{curElem.name}</h2>
+               <span className="card-author subtle">{category}</span>
+               <h2 className="card-title">{name}</h2>
                <span className="card-description subtle">
-               {curElem.description}
+               {description}
                </span>
                <div className="card-read">Read</div>
-
+              
           
-           <img src={curElem.image} alt="images" className="card-media" />
-           
+           <img src={image} alt="images" className="card-media" />
+           </div>
            <span className="card-tag subtle">Order Now</span>
            </div>
        </div>
-    </div>
+    
     </>
     );
         })}
--- src/component/menuApi.js
+++ src/component/menuApi.js
@@ -2,20 +2,20 @@ const Menu = [{
     id:1,
     image: "images/maggi.jpg",
     name: "maggie",
-    category: "Breakfast",
+    category: "breakfast",
     price:"Rs.25",
-    description: "Enjoy Cheese MAGGI Noodles, Paneer MAGGI Noodles, Egg MAGGI Noodles, Veg MAGGI Noodles that add a twist of flavour and yumminess to your everyday MAGGI Noodles. Recipes 21 Veggie Delight MAGGI Noodles Recipe 95 of 100 35 Min Easy Paneer Taka Tak MAGGI Noodles Recipe 55 of 100 36 Min Easy Paneer ...",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in .",
 },
 
 
 {
     id: 2,
     image: "../images/samosa.jpg",
-    name: "samaso",
-    category: "evening",
+    name: "Samosa",
+    category: "snacks",
     price: "Rs 10",
     description:
-      " Samosas are very delicious and a mouth watering snack. ",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in .",
   },
   {
     id: 3,
@@ -29,7 +29,7 @@ const Menu = [{
   {
     id: 4,
     image: "../images/chole-bhature.jpg",
-    name: "chole Bhature",
+    name: "Chole Bhature",
     category: "lunch",
     price: "Rs 50",
     description:
@@ -38,8 +38,8 @@ const Menu = [{
   {
     id: 5,
     image: "../images/pizza.jpg",
-    name: "pizza",
-    category: "evening",
+    name: "Pizza",
+    category: "snacks",
     price: "Rs 80",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
@@ -47,7 +47,7 @@ const Menu = [{
   {
     id: 6,
     image: "../images/non-veg_thali.jpg",
-    name: "Non-Veg Thali",
+    name: "Chicken Thali",
     category: "dinner",
     price: "Rs 180",
     description:
@@ -67,12 +67,109 @@ const Menu = [{
     image: "../images/rajma-chawal.jpg",
     name: "Rajma chawal",
     category: "lunch",
-    price: "60₹",
+    price: "Rs. 60",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
   },
-
-
+  {
+    id: 9,
+    image: "images/chaha.jpg",
+    name: "Chaha",
+    category: "breakfast",
+    price: "Rs 10",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 10,
+    image: "images/burger.jpg",
+    name:"Burger",
+    category: "snacks",
+    price: "Rs 50",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 11,
+    image: "images/coffe.jpg",
+    name: "Coffee",
+    category: "breakfast",
+    price: "Rs 15",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 12,
+    image: "images/donuts.jpg",
+    name: "Donuts",
+    category: "snacks",
+    price: "Rs. 15/ pc",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 13,
+    image: "images/momos.jpg",
+    name: "Momos",
+    category: "breakfast",
+    price: "Rs 30",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  }, 
+  {
+    id: 14,
+    image: "images/french-fries.jpg",
+    name: "French-fries",
+    category: "snacks",
+    price: "Rs 40",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  }, 
+  {
+    id: 15,
+    image: "images/pancake.jpg",
+    name: "Pancake",
+    category: "breakfast",
+    price: "Rs 30",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  }, 
+  {
+    id: 16,
+    image: "images/pastry.jpg",
+    name: "Pastry",
+    category: "snacks",
+    price: "Rs 40",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  }, 
+  {
+    id: 17,
+    image: "images/prawns.jpg",
+    name: "Fried Prawns",
+    category: "dinner",
+    price: "Rs 150",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  }, 
+  {
+    id: 18,
+    image: "images/smoothi.jpg",
+    name: "smoothi",
+    category: "snacks",
+    price: "Rs 80",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 19,
+    image: "images/sushi.jpg",
+    name: "Sushi",
+    category: "dinner",
+    price: "Rs 90",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  }, 
 ];
 
 
